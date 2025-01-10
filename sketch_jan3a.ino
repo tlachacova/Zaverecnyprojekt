7df3300f01678452273dc80db11fa0fdc201c4b0
tlachacova <simonka.tlachacova1@gmail.com>
2025-01-10 16:23:33 +0100

    Add files via upload

---
+++ sketch_jan3a.ino
@@ -0,0 +1,38 @@
+
+
+#include <DHT.h>
+#define DHT22_PIN  4 
+
+DHT dht22(DHT22_PIN, DHT22);
+
+void setup() {
+  Serial.begin(115200);
+  dht22.begin(); 
+}
+
+void loop() {
+  
+  float humi  = dht22.readHumidity();
+  float tempC = dht22.readTemperature();
+  float tempF = dht22.readTemperature(true);
+
+
+  if ( isnan(tempC) || isnan(tempF) || isnan(humi)) {
+    Serial.println("Failed to read from DHT22 sensor!");
+  } else {
+    Serial.print("Humidity: ");
+    Serial.print(humi);
+    Serial.print("%");
+
+    Serial.print("  |  ");
+
+    Serial.print("Temperature: ");
+    Serial.print(tempC);
+    Serial.print("°C  ~  ");
+    Serial.print(tempF);
+    Serial.println("°F");
+  }
+
+
+  delay(2000);
+  }
